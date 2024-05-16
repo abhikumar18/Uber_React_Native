686d6f90063061aa7ccec4088d75a1c95a208316
abhikumar18 <abhishek.it.msit@gmail.com>
2024-05-16 12:30:28 +0100

    Navigation

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,29 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView,Image } from 'react-native'
+import React from 'react';
+import tw, { style } from 'twrnc';
+import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
-const HomeScreen = () => {
+const HomeScreen = ({}) => {
   return (
-    <SafeAreaView>
-      <Text style={tw`text-red`}>I am Home Screen</Text>
+    <SafeAreaView style={tw`bg-white h-full`}>
+        <View  style={tw`p-5`}>
+            <Image 
+                source ={{
+                    uri:"https://links.papareact.com/gzs"
+                }}
+                style={{width:100,height:100,resizeMode:"contain"}}
+            />
+            <NavOptions />
+         </View>
     </SafeAreaView>
   )
 }
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    text:{
+        color:"blue",
+    }
+})
