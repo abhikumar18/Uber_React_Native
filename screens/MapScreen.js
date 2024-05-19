2d9617139836f7f14439f704cfb30bbb457bc143
abhikumar18 <abhishek.it.msit@gmail.com>
2024-05-19 12:55:02 +0100

    Map Implementation

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -3,15 +3,37 @@ import tw from 'twrnc';
 import React from 'react';
 import Maps from '../components/Maps';
 import MapView,{Marker} from 'react-native-maps';
+import NavigateCard from '../components/NavigateCard';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import RideOptionsCard from '../components/RideOptionsCard';
 
 const MapScreen = () => {
+  const Stack = createStackNavigator(); 
   return (
     <View>
       <View style={tw`h-1/2`}>
         <Maps />
       </View>
 
-      <View style={tw`h-1/2`}></View>
+      <View style={tw`h-1/2`}>
+         <Stack.Navigator>
+            <Stack.Screen
+               name="NavigateCard"
+               component={NavigateCard}
+               options={{
+                headerShown:false,
+               }}
+            />
+            <Stack.Screen
+               name="RideOptionsCard"
+               component={RideOptionsCard}
+               options={{
+                headerShown:false,
+               }}
+            />
+         </Stack.Navigator>
+      </View>
     </View>
   )
 } 
